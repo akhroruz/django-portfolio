6576b533d65066e4d9ff389a3493d37fc0dc4b40
Ahrorbek <admin@mail.ru>
2022-12-24 19:27:32 +0500

    add django-environ

--- apps/urls.py
+++ apps/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from apps.views import MessageView, CustomDetailView
 
 urlpatterns = [
     path('', MessageView.as_view(), name='index'),
     path('detail/<str:slug>', CustomDetailView.as_view(), name='detail'),
-    path('test', TemplateView.as_view(template_name='test.html'))
 ]
